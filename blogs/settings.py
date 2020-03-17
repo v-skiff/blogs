e1f9abae52ac087439a9a88f5aefa94d5c3c5476
skiff <skiff.udev@gmail.com>
2020-03-17 22:44:52 +0200

    Fix some data

--- blogs/settings.py
+++ blogs/settings.py
@@ -105,8 +105,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'v.skiff55@gmail.com'
-EMAIL_HOST_PASSWORD = '9&he@up*q4S7$8f5!CPtprPj'
+EMAIL_HOST_USER = 'someemail@gmail.com'
+EMAIL_HOST_PASSWORD = 'somepwd'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
